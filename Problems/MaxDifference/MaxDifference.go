3541b17af5d383589c906a5145aa034e4d0355f9
Akshay <akshay@Akshays-MacBook-Pro.local>
2019-01-25 23:03:51 +0530

    added code for maxDifference problem

---
+++ Problems/MaxDifference/MaxDifference.go
@@ -0,0 +1,35 @@
+// Given an array, find the pair with max difference such that larger number comes after smaller number
+// Example 1 : [2 3 10 2 4 7] => 8 (2,10)
+// Example 2 : [7 9 6 4 3 1] => 2 (7,9)
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var size int
+	fmt.Scanln(&size)
+
+	arr := []int{}
+	for i := 0; i < size; i++ {
+		var num int
+		fmt.Scanln(&num)
+		arr = append(arr, num)
+	}
+
+	maxDiff := 0
+	minEle := arr[0]
+
+	for _, v := range arr {
+		if v > minEle && v-minEle > maxDiff {
+			maxDiff = v - minEle
+		}
+
+		if minEle > v {
+			minEle = v
+		}
+	}
+
+	fmt.Println(maxDiff)
+}
